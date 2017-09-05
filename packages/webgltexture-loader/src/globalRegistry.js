3190fa27a6b2db64219f8cecc84dcf9793220933
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-09-05 11:18:22 +0200

    store globalRegistry in root so it's truely shared

--- packages/webgltexture-loader/src/globalRegistry.js
+++ packages/webgltexture-loader/src/globalRegistry.js
@@ -1,3 +1,8 @@
 import LoadersRegistry from "./LoadersRegistry";
+
+const root = typeof window === "undefined" ? global : window;
+const key = "__webglTextureLoader_registry";
+
 // This is a convenient loader registry that store all imported registry
-export default new LoadersRegistry();
+// it needs to be in global state so it does not get lost across potential many lib instances
+export default root[key] || (root[key] = new LoadersRegistry());
--- packages/webgltexture-loader/src/index.js
+++ packages/webgltexture-loader/src/index.js
@@ -6,20 +6,6 @@ import LoadersRegistry from "./LoadersRegistry";
 import LoaderResolver from "./LoaderResolver";
 import globalRegistry from "./globalRegistry";
 
-const root = typeof window === "undefined" ? global : window;
-if (root.__webglTextureLoaderLoaded) {
-  root.__webglTextureLoaderLoaded++;
-  console.warn(
-    "webgltexture-loader is loaded " +
-      (root.__webglTextureLoaderLoaded === 2
-        ? "twice"
-        : root.__webglTextureLoaderLoaded + " times") +
-      " in your bundle. This is a problem because the globalRegistry won't discover all loaders. Make sure your package manager / build system is configured properly to dedup."
-  );
-} else {
-  root.__webglTextureLoaderLoaded = 1;
-}
-
 export {
   globalRegistry,
   LoadersRegistry,
