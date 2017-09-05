c460b5353c2b31afed0cb507d4d43ec6e4ff8b51
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-09-05 10:12:34 +0200

    warn if lib loaded multiple times

--- packages/webgltexture-loader/src/index.js
+++ packages/webgltexture-loader/src/index.js
@@ -6,6 +6,19 @@ import LoadersRegistry from "./LoadersRegistry";
 import LoaderResolver from "./LoaderResolver";
 import globalRegistry from "./globalRegistry";
 
+if (global.__webglTextureLoaderLoaded) {
+  global.__webglTextureLoaderLoaded++;
+  console.warn(
+    "webgltexture-loader is loaded " +
+      (global.__webglTextureLoaderLoaded === 2
+        ? "twice"
+        : global.__webglTextureLoaderLoaded + " times") +
+      " in your bundle. This is a problem because the globalRegistry won't discover all loaders. Make sure your package manager / build system is configured properly to dedup."
+  );
+} else {
+  global.__webglTextureLoaderLoaded = 1;
+}
+
 export {
   globalRegistry,
   LoadersRegistry,
