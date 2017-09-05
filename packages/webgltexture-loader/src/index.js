a8883b34737b3f496f0d26c907f99a01f81c78d8
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-09-05 10:32:28 +0200

    safer root

--- packages/webgltexture-loader/src/index.js
+++ packages/webgltexture-loader/src/index.js
@@ -6,17 +6,18 @@ import LoadersRegistry from "./LoadersRegistry";
 import LoaderResolver from "./LoaderResolver";
 import globalRegistry from "./globalRegistry";
 
-if (global.__webglTextureLoaderLoaded) {
-  global.__webglTextureLoaderLoaded++;
+const root = typeof window === "undefined" ? global : window;
+if (root.__webglTextureLoaderLoaded) {
+  root.__webglTextureLoaderLoaded++;
   console.warn(
     "webgltexture-loader is loaded " +
-      (global.__webglTextureLoaderLoaded === 2
+      (root.__webglTextureLoaderLoaded === 2
         ? "twice"
-        : global.__webglTextureLoaderLoaded + " times") +
+        : root.__webglTextureLoaderLoaded + " times") +
       " in your bundle. This is a problem because the globalRegistry won't discover all loaders. Make sure your package manager / build system is configured properly to dedup."
   );
 } else {
-  global.__webglTextureLoaderLoaded = 1;
+  root.__webglTextureLoaderLoaded = 1;
 }
 
 export {
